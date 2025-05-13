34b23658e3ef42847b65df5fe538f5d64071045c
Asmaa <es-asmaa.ghanem2026@alexu.edu.eg>
2025-05-13 18:32:03 +0300

    Retransmission implemented, not tested

--- udp.py
+++ udp.py
@@ -15,7 +15,9 @@ class ReliableUDP:
     def __init__(self, timeout=2.0):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.seq = 0
-        self.timeout = timeout
+        self.ack_num = 0
+        self.timeout_val = timeout
+        self.server.settimeout(timeout)
 
     def bind(self,address):
         self.server.bind(address)
@@ -26,6 +28,40 @@ class ReliableUDP:
         cs = sum(data) % 256
         return struct.pack('!B', cs)  # 1 byte
     
+    # retransmission using ACK and timeout
+
+    def reliable_send(self,adr, payload):
+        while True:
+            pkt = self.make_packet(ACK, self.seq, payload)
+            self.server.sendto(pkt, adr)
+            try:
+                response, _ = self.server.recvfrom(1024)
+                parsed_pkt = self.parse_packet(response)
+
+                if parsed_pkt:
+                    flags, ack_seq, _ = parsed_pkt
+                    if (flags& ACK) and ack_seq==self.seq:
+                        self.seq ^=1 
+                        break
+            except socket.timeout:
+                print("timeout, waiting for retransmission")
+
+    def reliable_recv(self):
+        while True:
+            pkt, adr = self.server.recvfrom(1024)
+            parsed_pkt = self.parse_packet(pkt)
+            if parsed_pkt:
+                flags, seq, payload = parsed_pkt
+                if seq == self.ack:
+                    ack_pkt = self.make_packet(ACK, seq)
+                    self.server.sendto(ack_pkt, adr)
+                    self.ack ^= 1  
+                    return payload, adr
+                else:
+                    # Duplicate or out-of-order -> resend ACK for last valid
+                    print(f"Duplicate or out-of-order packet (seq={seq}). Resending ACK.")
+                    ack_pkt = self.make_packet(ACK, seq)
+                    self.server.sendto(ack_pkt, adr)
 
     def make_packet(self, flags, seq, payload=b''):
         # Packet structure:
@@ -36,9 +72,6 @@ class ReliableUDP:
         cs = self.checksum(body)
         return cs + body
 
-
-
-    
     def parse_packet(self, packet):
         
         if len(packet) < 6:  # 1 checksum + 1 flag + 4 seq
