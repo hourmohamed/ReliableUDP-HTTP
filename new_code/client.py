0c6a2e2c3ff3cbafebddeeadd299fef72d176e75
Tasneem <tasneemyosry700@gmail.com>
2025-05-18 20:44:33 +0300

    code fixed

--- new_code/client.py
+++ new_code/client.py
@@ -6,7 +6,7 @@ def main():
         print("[Client] Connected successfully")
 
         # Send data
-        message = b"GET /index.html HTTP/1.0\r\n\r\n"
+        message = b"GET /index.html HTTP/1.0 tas tas tas \r\n\r\n"
         client.send(message)
 
     client.close()
